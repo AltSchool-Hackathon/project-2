fbba373e457b417ddce9d81920b81b5ee2833edc
Nonso Andrew <nonsoandrew1@gmail.com>
2022-08-03 15:31:34 +0100

    Statistics fadded

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-let defaultEncrypt = "*****";
+let defaultEncrypt = "****";
+let totalWordCount;
+let totalWordMatchCount = 0;
 
 function getValue(event) {
+	let start = Date.now();
 	event.preventDefault();
 	let text =
 		document.getElementById("post").value;
@@ -13,6 +16,19 @@ function getValue(event) {
 	let newText =
 		document.getElementById("newText");
 
+	let wordCount =
+		document.getElementById("wordCount");
+	let matchCount =
+		document.getElementById("matchCount");
+	let scrambledCount = document.getElementById(
+		"scrambledCount"
+	);
+	let timeCount =
+		document.getElementById("timeCount");
+
+	// let toggler =
+	// 	document.getElementById("toggler");
+
 	let textArray = text.split(" ");
 	let textEncryptArray = textEncrypt.split(" ");
 	let textReplaceArray = textReplace.split(" ");
@@ -20,24 +36,21 @@ function getValue(event) {
 	// FUNCTION TO DETERMINE LENGTH OF SELECTED WORDS AND MAKE IT EQUAL TO LENGTH OF ASTERISK
 
 	// let asterisk = function getNoOfAsterisk(i) {
-	// 	for (
-	// 		let j = 0;
-	// 		j <= textEncryptArray[i].length;
-	// 		j++
-	// 	) {
-	// 		if (textReplaceArray[i]) {
-	// 			return;
-	// 		} else {
-	// 			defaultEncrypt += "*";
-	// 			console.log(textEncryptArray[i].length);
-	// 		}
-	// 	}
+	//  for(let j=0; j<)
+	// 	if (!textReplaceArray[i]) {
+	// 		console.log(defaultEncrypt);
+	//  }
 	// };
 
 	//Function that converts all text to lower
 
 	let changeCase = (text) => text.toLowerCase();
 
+	//TOGGLE ENCRYPTION TEXT
+
+	// function toggler (){
+	// 	textContent
+	// }
 	//Function to clear input field
 
 	let clearField = function () {
@@ -69,17 +82,28 @@ function getValue(event) {
 			i < textEncryptArray.length;
 			i++
 		) {
-			textArray.forEach((j, index) => {
-				if (j == textEncryptArray[i]) {
+			lowerTextArray.forEach((j, index) => {
+				if (j == lowerTextEncryptArray[i]) {
+					// asterisk(i);
 					textReplaceArray[i]
 						? (textArray[index] =
 								textReplaceArray[i])
 						: (textArray[index] = defaultEncrypt);
+					totalWordMatchCount += 1;
 				}
 			});
 		}
+		totalWordCount = textArray.length;
 
 		newText.textContent = textArray.join(" ");
+		wordCount.textContent = totalWordCount;
+		matchCount.textContent = totalWordMatchCount;
+		scrambledCount.textContent =
+			lowerTextEncryptArray.length;
+
+		let end = Date.now();
+
+		timeCount.textContent = `${end - start}ms`;
 	};
 
 	multiple();
